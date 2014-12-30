9a4342f9cc94850e8177df552341f9c5be3d1b0c
Brent Ertz <brent.ertz@gmail.com>
2014-12-30 16:15:36 -0700

    Extract app configuration from server creation

--- app/server/src/lib/app.js
+++ app/server/src/lib/app.js
@@ -8,20 +8,20 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var methodOverride = require('method-override');
-var errorHandler = require('./lib/error-handler');
+var errorHandler = require('./error-handler');
 var config = require('config');
-var routes = require('./routes/index');
-var services = require('./services/index');
+var routes = require('../routes/index');
+var services = require('../services/index');
 
 app.set('port', config.port);
 app.set('env', config.env);
 app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(methodOverride());
-//app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(config.get('paths.client.build.root')));
 
 if (app.get('env') === 'development') {
@@ -34,13 +34,5 @@ services(app, config);
 routes(app);
 
 app.use(errorHandler()); // Must appear after routes
-
-if (!module.parent) {
-  var port = app.get('port');
-  var env = app.get('env');
-  http.createServer(app).listen(port, function() {
-    console.log('Server listening in %s mode on port %d', env, port);
-  });
-}
 
 module.exports = app;
